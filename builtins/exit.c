2f8ca8dd508eb3fe2d131c511c9298fbbe8dddca
Malcolm <malxau@users.noreply.github.com>
2020-12-01 11:50:47 -0800

    Allow exit to specify exit code

--- builtins/exit.c
+++ builtins/exit.c
@@ -3,7 +3,7 @@
  *
  * Yori shell exit
  *
- * Copyright (c) 2017 Malcolm J. Smith
+ * Copyright (c) 2017-2020 Malcolm J. Smith
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,11 +29,35 @@
 #include <yoricall.h>
 
 /**
- Exit shell command
+ Help text to display to the user.
+ */
+const
+CHAR strExitHelpText[] =
+        "\n"
+        "Exit the shell optionally specifying exit code.\n"
+        "\n"
+        "EXIT [exitcode]\n";
+
+/**
+ Display usage text to the user.
+ */
+BOOL
+ExitHelp()
+{
+    YoriLibOutput(YORI_LIB_OUTPUT_STDOUT, _T("Exit %i.%02i\n"), YORI_VER_MAJOR, YORI_VER_MINOR);
+#if YORI_BUILD_ID
+    YoriLibOutput(YORI_LIB_OUTPUT_STDOUT, _T("  Build %i\n"), YORI_BUILD_ID);
+#endif
+    YoriLibOutput(YORI_LIB_OUTPUT_STDOUT, _T("%hs"), strExitHelpText);
+    return TRUE;
+}
+
+/**
+ The main entrypoint for the exit cmdlet.
 
  @param ArgC The number of arguments.
 
- @param ArgV The argument array.
+ @param ArgV An array of arguments.
 
  @return ExitCode, zero for success, nonzero for failure.
  */
@@ -44,12 +68,58 @@ YoriCmd_EXIT(
     __in YORI_STRING ArgV[]
     )
 {
-    UNREFERENCED_PARAMETER(ArgC);
-    UNREFERENCED_PARAMETER(ArgV);
+    BOOL ArgumentUnderstood;
+    DWORD i;
+    DWORD StartArg;
+    DWORD ExitCode;
+    YORI_STRING Arg;
+
     YoriLibLoadNtDllFunctions();
     YoriLibLoadKernel32Functions();
-    YoriCallExitProcess(EXIT_SUCCESS);
-    return EXIT_SUCCESS;
+
+    StartArg = 0;
+
+    for (i = 1; i < ArgC; i++) {
+
+        ArgumentUnderstood = FALSE;
+        ASSERT(YoriLibIsStringNullTerminated(&ArgV[i]));
+
+        if (YoriLibIsCommandLineOption(&ArgV[i], &Arg)) {
+
+            if (YoriLibCompareStringWithLiteralInsensitive(&Arg, _T("?")) == 0) {
+                ExitHelp();
+                return EXIT_SUCCESS;
+            } else if (YoriLibCompareStringWithLiteralInsensitive(&Arg, _T("license")) == 0) {
+                YoriLibDisplayMitLicense(_T("2017-2020"));
+                return EXIT_SUCCESS;
+            }
+        } else {
+            StartArg = i;
+            ArgumentUnderstood = TRUE;
+            break;
+        }
+
+        if (!ArgumentUnderstood) {
+            YoriLibOutput(YORI_LIB_OUTPUT_STDERR, _T("Argument not understood, ignored: %y\n"), &ArgV[i]);
+        }
+    }
+
+    if (StartArg == 0) {
+        ExitCode = EXIT_SUCCESS;
+    } else {
+        LONGLONG llTemp;
+        DWORD CharsConsumed;
+        if (!YoriLibStringToNumber(&ArgV[StartArg], FALSE, &llTemp, &CharsConsumed) ||
+            CharsConsumed == 0) {
+
+            return EXIT_FAILURE;
+        }
+
+        ExitCode = (DWORD)llTemp;
+    }
+
+    YoriCallExitProcess(ExitCode);
+    return ExitCode;
 }
 
 // vim:sw=4:ts=4:et:
