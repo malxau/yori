7976c47ae137ac3c669de124e459fbda3e8f0242
Malcolm <malxau@users.noreply.github.com>
2023-01-01 17:00:10 -0800

    Bump version

--- config/ver.mk
+++ config/ver.mk
@@ -1,13 +1,13 @@
 
 !IFNDEF YORI_BASE_VER_MAJOR
 YORI_BASE_VER_MAJOR=1
-YORI_BASE_VER_MINOR=80
+YORI_BASE_VER_MINOR=90
 
 # Which previously shipped outside Yori with a version of 2.0.
 # To maintain this legacy, it gets its own version.
 
 WHICH_VER_MAJOR=2
-WHICH_VER_MINOR=17
+WHICH_VER_MINOR=18
 
 YORI_VER_MAJOR=$(YORI_BASE_VER_MAJOR)
 YORI_VER_MINOR=$(YORI_BASE_VER_MINOR)
