aaf13dbac9b5fd461e8ebdad2f74a3be127d1e76
Malcolm <malxau@users.noreply.github.com>
2023-02-28 17:00:30 -0800

    Optionally allow tee to output directly to the console, so stdout can be processed before sending to a file

--- tee/tee.c
+++ tee/tee.c
@@ -3,7 +3,7 @@
  *
  * Yori shell output to a file and stdout
  *
- * Copyright (c) 2017-2018 Malcolm J. Smith
+ * Copyright (c) 2017-2023 Malcolm J. Smith
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,9 +35,11 @@ CHAR strTeeHelpText[] =
         "\n"
         "Output the contents of standard input to standard output and a file.\n"
         "\n"
-        "TEE [-license] [-a] <file>\n"
+        "TEE [-license] -c\n"
+        "TEE [-license] [-a] <file>]\n"
         "\n"
-        "   -a             Append to the file\n";
+        "   -a             Append to the file\n"
+        "   -c             Write to the console and standard output\n";
 
 /**
  Display usage text to the user.
@@ -65,7 +67,52 @@ typedef struct _TEE_CONTEXT {
      */
     HANDLE hFile;
 
+    /**
+     TRUE if hFile is a handle to a console; FALSE if it is a handle to a
+     different type of device.
+     */
+    BOOLEAN FileIsConsole;
+
 } TEE_CONTEXT, *PTEE_CONTEXT;
+
+/**
+ Write a single line to an output device.
+
+ @param hDevice Handle to the output device.
+
+ @param IsConsole TRUE if the output device is a console; FALSE if it is not.
+        This is used to determine whether a newline needs to be inserted in
+        cases where the console has advanced to the next line automatically.
+
+ @param Line Pointer to the line to output.
+ */
+VOID
+TeeWriteLine(
+    __in HANDLE hDevice,
+    __in BOOLEAN IsConsole,
+    __in PYORI_STRING Line
+    )
+{
+    CONSOLE_SCREEN_BUFFER_INFO ScreenInfo;
+
+    //
+    //  If it's not a console, output the line and newline in one operation.
+    //  If it is a console, output the line, and see where the cursor is,
+    //  then optionally include a newline.
+    //
+
+    if (!IsConsole) {
+        YoriLibOutputToDevice(hDevice, 0, _T("%y\n"), Line);
+    } else {
+        YoriLibOutputToDevice(hDevice, 0, _T("%y"), Line);
+        if (Line->LengthInChars == 0 ||
+            !GetConsoleScreenBufferInfo(hDevice, &ScreenInfo) ||
+            ScreenInfo.dwCursorPosition.X != 0) {
+
+            YoriLibOutputToDevice(hDevice, 0, _T("\n"));
+        }
+    }
+}
 
 /**
  Process a single stream.
@@ -84,8 +131,17 @@ TeeProcessStream(
     )
 {
     PVOID LineContext = NULL;
-    CONSOLE_SCREEN_BUFFER_INFO ScreenInfo;
+    DWORD Junk;
     YORI_STRING LineString;
+    HANDLE StdOutHandle;
+    BOOLEAN StdOutIsConsole;
+
+    StdOutIsConsole = FALSE;
+
+    StdOutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (GetConsoleMode(StdOutHandle, &Junk)) {
+        StdOutIsConsole = TRUE;
+    }
 
     YoriLibInitEmptyString(&LineString);
 
@@ -95,12 +151,8 @@ TeeProcessStream(
             break;
         }
 
-        YoriLibOutput(YORI_LIB_OUTPUT_STDOUT, _T("%y"), &LineString);
-        if (LineString.LengthInChars == 0 || !GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ScreenInfo) || ScreenInfo.dwCursorPosition.X != 0) {
-            YoriLibOutput(YORI_LIB_OUTPUT_STDOUT, _T("\n"));
-        }
-
-        YoriLibOutputToDevice(TeeContext->hFile, 0, _T("%y\n"), &LineString);
+        TeeWriteLine(StdOutHandle, StdOutIsConsole, &LineString);
+        TeeWriteLine(TeeContext->hFile, TeeContext->FileIsConsole, &LineString);
     }
 
     YoriLibLineReadCloseOrCache(LineContext);
@@ -140,7 +192,9 @@ ENTRYPOINT(
     BOOL ArgumentUnderstood;
     DWORD i;
     DWORD StartArg = 0;
-    BOOL Append = FALSE;
+    DWORD DesiredAccess;
+    BOOLEAN Append = FALSE;
+    BOOLEAN Console = FALSE;
     TEE_CONTEXT TeeContext;
     YORI_STRING FileName;
     YORI_STRING Arg;
@@ -158,10 +212,13 @@ ENTRYPOINT(
                 TeeHelp();
                 return EXIT_SUCCESS;
             } else if (YoriLibCompareStringWithLiteralInsensitive(&Arg, _T("license")) == 0) {
-                YoriLibDisplayMitLicense(_T("2017-2018"));
+                YoriLibDisplayMitLicense(_T("2017-2023"));
                 return EXIT_SUCCESS;
             } else if (YoriLibCompareStringWithLiteralInsensitive(&Arg, _T("a")) == 0) {
                 Append = TRUE;
+                ArgumentUnderstood = TRUE;
+            } else if (YoriLibCompareStringWithLiteralInsensitive(&Arg, _T("c")) == 0) {
+                Console = TRUE;
                 ArgumentUnderstood = TRUE;
             } else if (YoriLibCompareStringWithLiteralInsensitive(&Arg, _T("-")) == 0) {
                 StartArg = i + 1;
@@ -184,27 +241,40 @@ ENTRYPOINT(
     //  the file and use that
     //
 
-    if (StartArg == 0 || StartArg == ArgC) {
+    if (!Console && (StartArg == 0 || StartArg == ArgC)) {
         YoriLibOutput(YORI_LIB_OUTPUT_STDERR, _T("tee: argument missing\n"));
         return EXIT_FAILURE;
     }
 
     if (YoriLibIsStdInConsole()) {
-        YoriLibOutput(YORI_LIB_OUTPUT_STDERR, _T("No file or pipe for input\n"));
+        YoriLibOutput(YORI_LIB_OUTPUT_STDERR, _T("tee: No file or pipe for input\n"));
         return EXIT_FAILURE;
     }
 
-    if (!YoriLibUserStringToSingleFilePath(&ArgV[StartArg], TRUE, &FileName)) {
-        DWORD LastError = GetLastError();
-        LPTSTR ErrText = YoriLibGetWinErrorText(LastError);
-        YoriLibOutput(YORI_LIB_OUTPUT_STDERR, _T("tee: getfullpathname of %y failed: %s"), &ArgV[StartArg], ErrText);
-        YoriLibFreeWinErrorText(ErrText);
-        return EXIT_FAILURE;
+    if (Console) {
+        YoriLibConstantString(&FileName, _T("CONOUT$"));
+        TeeContext.FileIsConsole = TRUE;
+
+        //
+        //  Open for read and write so we can query the cursor location.
+        //
+
+        DesiredAccess = GENERIC_READ | GENERIC_WRITE;
+    } else {
+
+        if (!YoriLibUserStringToSingleFilePath(&ArgV[StartArg], TRUE, &FileName)) {
+            DWORD LastError = GetLastError();
+            LPTSTR ErrText = YoriLibGetWinErrorText(LastError);
+            YoriLibOutput(YORI_LIB_OUTPUT_STDERR, _T("tee: getfullpathname of %y failed: %s"), &ArgV[StartArg], ErrText);
+            YoriLibFreeWinErrorText(ErrText);
+            return EXIT_FAILURE;
+        }
+        DesiredAccess = (Append?FILE_APPEND_DATA:FILE_WRITE_DATA) | SYNCHRONIZE;
     }
 
     TeeContext.hFile = CreateFile(FileName.StartOfString,
-                                  (Append?FILE_APPEND_DATA:FILE_WRITE_DATA) | SYNCHRONIZE,
-                                  FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,
+                                  DesiredAccess,
+                                  FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
                                   NULL,
                                   OPEN_ALWAYS,
                                   FILE_ATTRIBUTE_NORMAL,
